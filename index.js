b9026f0d16b1a02080a507f31f8801e8bf094881
ZYYNN <zhuynchn@163.com>
2017-09-23 21:02:40 +0800

    express,ejs

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
-var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
 var userRouter = require('./routes/user');
 
-app.use('/', indexRouter);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', homeRouter);
 app.use('/user', userRouter);
 
 app.listen(3000);
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:name', function(req, res) {
-	res.send('user ' + req.params.name);
+	res.render('user', {name: req.params.name});
 });
 
 module.exports = router;
