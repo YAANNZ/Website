1b28ab699253cb322357cc7a0394cb9ef7707324
ZYYNN <zhuynchn@163.com>
2017-10-02 23:41:55 +0800

    mongodb

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 routes(app);
-// app.use('/home', require('./routes/home'));
-// app.use('/user', require('./routes/user'));
 
 app.listen(3000);
+
+
+// var MongoClient = require('mongodb').MongoClient,
+//   test = require('assert');
+// // Connection url
+// var url = 'mongodb://localhost:27017/test';
+// // Connect using MongoClient
+// MongoClient.connect(url, function(err, db) {
+//   // Use the admin database for the operation
+//   var adminDb = db.admin();
+//   // List all the available databases
+//   adminDb.listDatabases(function(err, dbs) {
+//     test.equal(null, err);
+//     test.ok(dbs.databases.length > 0);
+//     db.close();
+//   });
+// });
