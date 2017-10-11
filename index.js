422779a219dae9227f9b05692c4eff9165de4819
ZYYNN <zhuynchn@163.com>
2017-10-11 15:31:18 +0800

    config-lite

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var routes = require('./routes');
+var config = require('config-lite')(__dirname);
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -10,7 +11,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 routes(app);
 
-app.listen(3000);
+// app.listen(3000);
+app.listen(config.port);
 
 
 // var MongoClient = require('mongodb').MongoClient,
@@ -29,24 +31,3 @@ app.listen(3000);
 //   });
 // });
 
-var MongoClient = require('mongodb').MongoClient,
-  test = require('assert');
-// Connection url
-var url = 'mongodb://localhost:27017/test';
-// Connect using MongoClient
-MongoClient.connect(url, function(err, db) {
-  // Create a collection we want to drop later
-  var col = db.collection('createIndexExample1');
-  // Insert a bunch of documents
-  col.insert([{a:1, b:1}
-    , {a:2, b:2}, {a:3, b:3}
-    , {a:4, b:4}], {w:1}, function(err, result) {
-    test.equal(null, err);
-    // Show that duplicate records got dropped
-    col.aggregation({}, {cursor: {}}).toArray(function(err, items) {
-      test.equal(null, err);
-      test.equal(4, items.length);
-      db.close();
-    });
-  });
-});
