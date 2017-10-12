ba5adab3d7cf6d726e124bc6dfbc47e05ae21894
ZYYNN <zhuynchn@163.com>
2017-10-12 23:10:32 +0800

    index.js

--- index.js
+++ index.js
@@ -15,19 +15,3 @@ routes(app);
 app.listen(config.port);
 
 
-// var MongoClient = require('mongodb').MongoClient,
-//   test = require('assert');
-
-// var url = 'mongodb://localhost:27017/test';
-
-// MongoClient.connect(url, function(err, db) {
-//   // Use the admin database for the operation
-//   var adminDb = db.admin();
-//   // List all the available databases
-//   adminDb.listDatabases(function(err, dbs) {
-//     test.equal(null, err);
-//     test.ok(dbs.databases.length > 0);
-//     db.close();
-//   });
-// });
-
